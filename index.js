e0f773d4d7d509a8f0c4b2a036e03f10663ba688
corniceinfosoft <corniceinfo.soft.sol@gmail.com>
2021-09-09 17:51:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// console.log("Hello everyone")
+// var x=5;
+// var y=6
+// var z=x+y
+// console.log(z)
+arrayobject=[
+    {'Name':'Kinal' , 'Course':'B.tech', 'Branch' : 'Cse'}
+]
+
+
+function addarray()
+{
+    var el = document.getElementById("Demo");
+    console.log(el)
+}
+
+addarray();
+console.log(arrayobject)
